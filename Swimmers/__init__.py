5aec11208df0a78dd415d85b3ebba6b3b88e4d19
UMAR15071 <syedmumer15071@gmail.com>
2023-11-14 23:17:13 +0000

    secound commit

--- Swimmers/__init__.py
+++ Swimmers/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from Swimmers import swim_utils
 
 app = Flask(__name__)
+app.secret_key = 'a_secret_key'
 
 from Swimmers import route
 
 if(__name__ == "__main__"):
     app.run(debug = True)
+    
--- Swimmers/route.py
+++ Swimmers/route.py
@@ -1,7 +1,8 @@
 from Swimmers import app
-from flask import render_template, request
+from flask import render_template, request, session
 import os
 from Swimmers.swim_utils import get_swimmers_data
+from Swimmers.hfpy_utils import convert2range
 
 FOLDER = 'swimdata'
 files = os.listdir(FOLDER)
@@ -12,8 +13,6 @@ for file in files:
     data = []
     data = get_swimmers_data(file)
     names.add(data[0])
-
-
 
 @app.route('/')
 def name_page():
@@ -26,6 +25,7 @@ def event_name():
     events = set()
     if request.method == 'POST':
         selected_name = request.form.get("name")
+        session['selected_name'] = selected_name
         
         for file in files:
             data = get_swimmers_data(file)
@@ -33,3 +33,28 @@ def event_name():
                 events.add(data[1] + " " +data[2] + " " + data[3])
 
     return render_template('Events.html', events = events)
+
+
+@app.route('/show_graphs', methods = ['GET', 'POST'])
+def show_graphs():
+
+    selected_name = session.get('selected_name')
+
+    if request.method == 'POST':
+        swimmer = (selected_name + " " + request.form.get('event')).strip().split(" ")
+        name,age,distance,stroke = swimmer
+        filename = f"{name}-{age}-{distance}-{stroke}.txt"
+        data = get_swimmers_data(filename)
+        name, age, distance, stroke, times, values, average = data
+
+        title = f"{name} (Under {age}) - {distance} - {stroke}"
+
+        converts = []
+        for n in values:
+            converts.append(convert2range(n,0,max(values) + 50, 0, 400))
+
+        times.reverse() 
+        converts.reverse() 
+        tc = zip(times,converts)
+        
+    return render_template('Graphs.html', title = title, tc = tc, average = average)
